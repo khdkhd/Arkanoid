b5e82aae84d7d77cf314022d2cebdad277373f15
NealRame <contact@nealrame.com>
2016-10-12 00:12:19 +0200

    implement gulp sass tasks

--- tools/gulp/tasks/sass.js
+++ tools/gulp/tasks/sass.js
@@ -1,13 +1,29 @@
+const del = require('del');
+const env = require('../env');
 const gulp = require('gulp');
+const gulp_if = require('gulp-if');
+const livereload = require('gulp-livereload');
+const path = require('path');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', () => {
-});
+const output_dir = 'public/assets/sass';
+const sources_dir = 'sources/sass';
 
-gulp.task('sass-clean', () => {
-});
+gulp.task('sass-clean', () => del(output_dir));
 
-gulp.task('sass-watch', () => {
-});
+gulp.task('sass', () => gulp.src(path.join(sources_dir, '**.scss'))
+	.pipe(gulp_if(env.isDevelopment, sourcemaps.init()))
+	.pipe(sass({
+		includePaths: [sources_dir],
+		outputStyle: 'compressed'
+	})).on('error', sass.logError))
+	.pipe(gulp_if(env.isDevelopment, sourcemaps.write()))
+	.pipe(gulp.dest(output_dir))
+	.pipe(livereload()
+);
+
+gulp.task('sass-watch', ['sass'], () => gulp.watch(path.join(sources_dir, '**/*.scss'), ['sass']));
 
 module.exports = {
 	build: 'sass',
