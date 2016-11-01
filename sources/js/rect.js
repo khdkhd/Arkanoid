5bd82dffa955bae486d6a74607585fa37e106d8d
NealRame <contact@nealrame.com>
2016-11-01 16:22:41 +0100

    change interface

--- sources/js/rect.js
+++ sources/js/rect.js
@@ -1,11 +1,6 @@
 import createVector from 'vector';
 
-export default function createRect({
-	x,
-	y,
-	width,
-	height
-}) {
+export default function createRect({x, y}, {width, height}) {
 	return {
 		get x() {
 			return x;
@@ -19,36 +14,56 @@ export default function createRect({
 		get height() {
 			return height;
 		},
+		get size() {
+			return {
+				width,
+				height
+			};
+		},
 		get topLeft() {
-			return createVector({x:x, y:y});
+			return createVector({
+				x: x,
+				y: y
+			});
 		},
 		get topRight() {
-			return createVector({x:x + width, y:y});
+			return createVector({
+				x: x + width,
+				y: y
+			});
 		},
 		get bottomLeft() {
-			return createVector({x:x, y:y + height});
+			return createVector({
+				x: x,
+				y: y + height
+			});
 		},
 		get bottomRight() {
-			return createVector({x:x + width, y:y + height});
+			return createVector({
+				x: x + width,
+				y: y + height
+			});
 		},
-		get center(){
-			return createVector({x: x + width/2, y: y + height/2});
+		get center() {
+			return createVector({
+				x: x + width/2,
+				y: y + height/2
+			});
 		},
-		contains({x,y}){
-			return x >= this.bottomLeft.x
-				&& y <= this.bottomLeft.y
-				&& x >= this.topLeft.x
-				&& y > this.topLeft.y
-				&& x < this.topRight.x
-				&& y > this.topRight.y
-				&& x < this.bottomRight.x
-				&& y < this.bottomRight.y;
+		contains({x, y}) {
+			return x >= this.bottomLeft.x  && y <= this.bottomLeft.y
+				&& x >= this.topLeft.x     && y  > this.topLeft.y
+				&& x  < this.topRight.x    && y  > this.topRight.y
+				&& x  < this.bottomRight.x && y  < this.bottomRight.y;
 		},
 		intersect(rect) {
 			return this.contains(rect.topRight)
-					|| this.contains(rect.bottomRight)
-					|| this.contains(rect.bottomLeft)
-					|| this.contains(rect.topLeft);
+				|| this.contains(rect.bottomRight)
+				|| this.contains(rect.bottomLeft)
+				|| this.contains(rect.topLeft);
+		},
+		translate({x, y}) {
+			return createRect(this.topLeft.add({x, y}), {width, height});
 		}
 	};
 }
