5c88065dd554748783351791a2fbe95448aba4e8
NealRame <contact@nealrame.com>
2016-11-02 02:41:05 +0100

    indent

--- sources/js/brick.js
+++ sources/js/brick.js
@@ -4,9 +4,7 @@ import Rect from 'rect';
 function createBrick({x, y}, screen) {
 	const bottom_outer_rect = Rect({x, y}, {width: 2, height: 1});
 	const top_outer_rect = Rect({x, y}, {width: 1.8, height: .8});
-
 	const inner_rect = Rect({x: x + .2, y: y + .2}, {width: 1.6, height: .6});
-
 	const scale_factor = (screen.width/14)/2;
 	const line_width = 1/scale_factor;
 	return {
