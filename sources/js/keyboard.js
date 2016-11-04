743c65f122c19120b433aeaaaf76b23e245d3833
NealRame <contact@nealrame.com>
2016-11-04 19:35:27 +0100

    disable/enable auto repeat event

--- sources/js/keyboard.js
+++ sources/js/keyboard.js
@@ -8,19 +8,88 @@ import Vector from 'vector';
 const keyboard = Object.assign(Object.create(new EventEmitter()), {
 });
 
-const keyboard_event = cond([
-	[key => key === 32, constant(['pause'])],
-	[key => key === 37, constant(['direction-changed', Vector({x: -1, y: 0})])],
-	[key => key === 39, constant(['direction-changed', Vector({x:  1, y: 0})])]
-]);
-
-document.addEventListener('keydown', ev => {
-	const event_data = keyboard_event(ev.keyCode);
-	if (!is_nil(event_data)) {
-		keyboard.emit.apply(keyboard, event_data);
-		ev.preventDefault();
-		ev.stopPropagation();
+function create_key_handler({
+	code,
+	event,
+	on_keydown,
+	on_keyup,
+	repeat = true}) {
+	let pressed = false;
+	return {
+		code,
+		keydown(arg) {
+			if (!pressed || repeat) {
+				pressed = true;
+				const data = on_keydown(arg);
+				return Object.assign({event}, is_nil(data) ? {} : {data});
+			}
+		},
+		keyup(arg) {
+			pressed = false;
+			const data = on_keyup(arg);
+			return Object.assign({event}, is_nil(data) ? {} : {data});
+		}
 	}
-});
+}
+
+function keydown_handler(key_handlers) {
+	const handle = cond(key_handlers.map(handler => [
+		key => key === handler.code,
+		handler.keydown
+	]));
+	return ev => {
+		const event_data = handle(ev.keyCode);
+		if (!is_nil(event_data)) {
+			keyboard.emit(event_data.event, event_data.data);
+			ev.preventDefault();
+			ev.stopPropagation();
+		}
+	};
+}
+
+function keyup_handler(key_handlers) {
+	const handle = cond(key_handlers.map(handler => [
+		key => key === handler.code,
+		handler.keyup
+	]));
+	return ev => {
+		const event_data = handle(ev.keyCode);
+		if (!is_nil(event_data)) {
+			keyboard.emit(event_data.event, event_data.data);
+			ev.preventDefault();
+			ev.stopPropagation();
+		}
+	}
+}
+
+const LEFT_ARROW_KEY = 37;
+const RIGHT_ARROW_KEY = 39;
+const SPACE_BAR_KEY = 32;
+
+const keydown_handlers = [
+	create_key_handler({
+		code: LEFT_ARROW_KEY,
+		event: 'direction-changed',
+		on_keydown: constant(Vector.Left),
+		on_keyup: constant(Vector.Null),
+		repeat: false
+	}),
+	create_key_handler({
+		code: RIGHT_ARROW_KEY,
+		event: 'direction-changed',
+		on_keydown: constant(Vector.Right),
+		on_keyup: constant(Vector.Null),
+		repeat: false
+	}),
+	create_key_handler({
+		code: SPACE_BAR_KEY,
+		event: 'fire',
+		on_keydown: constant(true),
+		on_keyup: constant(false)
+	})
+];
+
+document.addEventListener('keydown', keydown_handler(keydown_handlers));
+document.addEventListener('keyup', keyup_handler(keydown_handlers));
 
 export default keyboard;
