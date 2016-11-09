05c155862842581498e7eec0ba359ae252ee4ca1
NealRame <contact@nealrame.com>
2016-11-09 01:01:58 +0100

    ball is da place!

--- sources/js/game.js
+++ sources/js/game.js
@@ -1,6 +1,7 @@
 import Rect from 'rect';
 import Vector from 'vector';
 import Brick from 'brick';
+import Ball from 'ball';
 import Vaus from 'vaus';
 import {
 	HorizontalWall,
@@ -27,6 +28,11 @@ const colors = [
 	'gray',
 	'gold'
 ];
+
+function createBall(vaus, scale_factor) {
+	const vaus_box = vaus.bbox;
+	return Ball({x: vaus_box.center.x, y: vaus_box.topY - Ball.Radius}, scale_factor);
+}
 
 function createBricks(cols, rows, scale) {
 	const bricks = [];
@@ -69,25 +75,66 @@ export default function createGame() {
 	const rows = screen.height/scale_factor;
 
 	const zone = Rect({x: 0, y: 0}, {width: columns - 2, height: rows - 2});
-	const vaus = Vaus({x: 0, y: zone.height - 2}, scale_factor);
-	const bricks = createBricks((columns - 2)/2, 7, scale_factor);
-	const walls = createWalls(columns - 1, rows);
 
+	const walls = createWalls(columns - 1, rows);
+	const bricks = createBricks((columns - 2)/2, 7, scale_factor);
+	const vaus = Vaus({x: 1, y: zone.height - 2}, scale_factor);
+
+	let ball = createBall(vaus, scale_factor);
 	let vaus_speed = Vector.Null;
+	let ball_speed = Vector.Null;
 
 	keyboard.on('direction-changed', direction => {
 		vaus_speed = direction.mul(.4);
 	});
+	keyboard.on('fire', () => {
+		if (ball_speed.isNull()) {
+			ball_speed = Vector({x: 1, y: -1}).toUnit().mul(.2);
+		}
+	});
 
 	// Move helpers
+
+	function vaus_ball_collide(vaus_box, ball_box) {
+		return vaus_box.intersect(ball_box)
+			&& vaus_box.leftX < ball_box.leftX
+			&& vaus_box.rightX > ball_box.rightX;
+	}
+
+	function move_ball() {
+		if (!ball_speed.isNull()) {
+			const ball_box = ball.bbox.translate(ball_speed);
+			const vaus_box = vaus.bbox;
+
+			if (ball_box.leftX <= zone.leftX) {
+				// colide with left wall
+				ball_speed = Vector({x: -ball_speed.x, y: ball_speed.y});
+			} else if (ball_box.rightX >= zone.rightX) {
+				// colide with right wall
+				ball_speed = Vector({x: -ball_speed.x, y: ball_speed.y});
+			} else if (ball_box.topY <= zone.topY) {
+				// collision with roof
+				ball_speed = Vector({x: ball_speed.x, y: -ball_speed.y});
+			} else if (vaus_ball_collide(vaus_box, ball_box)) {
+				ball_speed = Vector({x: ball_speed.x, y: -ball_speed.y});
+			} else if (ball_box.bottomY >= zone.bottomY) {
+				ball_speed = Vector.Null;
+				ball = createBall(vaus);
+			}
+			ball.move(ball_speed);
+		} else {
+			ball.move(vaus_speed);
+		}
+	}
 
 	function move_vaus() {
 		const box = vaus.bbox.translate(vaus_speed);
 		if (box.topLeft.x > 0 && vaus_speed.x < 0) {
 			vaus.move(vaus_speed);
-		}
-		if (box.topRight.x < zone.width && vaus_speed.x > 0) {
+		} else if (box.topRight.x < zone.width && vaus_speed.x > 0) {
 			vaus.move(vaus_speed);
+		} else {
+			vaus_speed = Vector.Null;
 		}
 	}
 
@@ -111,6 +158,13 @@ export default function createGame() {
 		}
 	}
 
+	function draw_ball() {
+		screen.save();
+		screen.translate(ball.pos);
+		ball.draw(screen);
+		screen.restore();
+	}
+
 	function draw_vaus() {
 		screen.save();
 		screen.translate(vaus.pos);
@@ -121,12 +175,18 @@ export default function createGame() {
 	function draw() {
 		screen.brush = '#123';
 		screen.clear();
+
 		screen.save();
 		screen.scale(scale_factor);
+
 		draw_walls();
+
 		screen.translate({x: 1, y: 1});
+
 		draw_bricks();
 		draw_vaus();
+		draw_ball();
+
 		screen.restore();
 	}
 
@@ -134,6 +194,7 @@ export default function createGame() {
 
 	function loop() {
 		move_vaus();
+		move_ball();
 		draw();
 		requestAnimationFrame(loop);
 	}
