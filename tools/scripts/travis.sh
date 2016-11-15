34cf72d4d8c11d35b0992574b4bf7f5f3afbe4f3
NealRame <contact@nealrame.com>
2016-11-15 23:25:17 +0100

    only build and test master and develop branches

---
+++ tools/scripts/travis.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# stop script with non-zero exit code if anything go wrong
+set -e
+
+# stop script with non-zero exit code when trying to reference an undefined
+# variable
+set -u
+
+# If any command in a pipeline fails, that return code will be used as the
+# return code of the whole pipeline
+set -o pipefail
+
+if [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "develop" ];
+	then
+	npm run build
+	npm run test
+fi
