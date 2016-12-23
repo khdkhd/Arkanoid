6e29d534ff51bc34d79dc8b390ef22823aa7639a
NealRame <contact@nealrame.com>
2016-12-23 20:27:07 +0100

    deploy script for travis

---
+++ tools/scripts/deploy.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# exit with non-zero exit code if anything fails
+set -e
+
+USER_NAME="Travis CI"
+USER_EMAIL="julien@graziano.fr"
+
+GH_REF="${GH_REF:-khdkhd/arkanoid.github.io.git}"
+DATE=$(date +"%m.%d.%y")
+SHA=$(git rev-parse HEAD)
+
+pushd "$DEST_DIR"
+	git init
+	git config user.name "$USER_NAME"
+	git config user.email "$USER_EMAIL"
+	git add .
+	git commit -F- <<-EOF
+		Deploy $DATE
+		commit: khdkhd/arkanoid.github.io.git
+	EOF
+	git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}" master:master > /dev/null 2>&1
+popd
