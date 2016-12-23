5e543f2f7f057e18faa72bf135cc8f58fdc1a26b
NealRame <contact@nealrame.com>
2016-12-23 22:43:30 +0100

    update deploy script to deploy on remote server

--- tools/scripts/deploy.sh
+++ tools/scripts/deploy.sh
@@ -3,21 +3,26 @@
 # exit with non-zero exit code if anything fails
 set -e
 
-USER_NAME="Travis CI"
-USER_EMAIL="julien@graziano.fr"
+# stop script with non-zero exit code when trying to reference an undefined
+# variable
+set -u
 
-GH_REF="${GH_REF:-khdkhd/arkanoid.github.io.git}"
-DATE=$(date +"%m.%d.%y")
+# If any command in a pipeline fails, that return code will be used as the
+# return code of the whole pipeline
+set -o pipefail
+
+export SSHPASS="$DEPLOY_PASSWORD"
+export SSH_OPTIONS="-o stricthostkeychecking=no"
+
+SCP="sshpass -e scp $SSH_OPTIONS"
+SSH="sshpass -e ssh $SSH_OPTIONS"
+
 SHA=$(git rev-parse HEAD)
 
 pushd "$DEST_DIR"
-	git init
-	git config user.name "$USER_NAME"
-	git config user.email "$USER_EMAIL"
-	git add .
-	git commit -F- <<-EOF
-		Deploy $DATE
-		commit: khdkhd/Arkanoid@$SHA
-	EOF
-	git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}" master:master > /dev/null 2>&1
+	tar czvf ../package.tgz .
 popd
+
+$SCP package.tgz "$DEPLOY_USER@DEPLOY_HOST:$DEPLOY_DIR" > /dev/null 2>&1
+$SCP tools/scripts/remote-deploy.sh "$DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIR" > /dev/null 2>&1
+$SSH "$DEPLOY_USER@$DEPLOY_HOST" "$DEPLOY_DIR/remote-deploy.sh" "$SHA" > /dev/null 2>&1
