75e8fc1baf4cfe1fa8dad8bb018b65442e9e1bb3
NealRame <contact@nealrame.com>
2016-12-24 02:03:14 +0100

    always build

--- tools/scripts/travis.sh
+++ tools/scripts/travis.sh
@@ -14,6 +14,7 @@ set -o pipefail
 if [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "develop" ];
 	then
 	./node_modules/.bin/eslint sources
-	npm run build
 	npm run test
 fi
+
+npm run build
