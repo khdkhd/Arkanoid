d05cccaee509a09c28b00b270fc53d82ab246d18
NealRame <contact@nealrame.com>
2017-01-07 00:40:28 +0100

    refactoring

--- sources/js/ui/keyboard.js
+++ sources/js/ui/keyboard.js
@@ -2,6 +2,8 @@ import cond from 'lodash.cond';
 import is_nil from 'lodash.isnil';
 import EventEmitter from 'events';
 
+import {completeAssign} from 'common/utils';
+
 function create_key_handler({
 	code,
 	event,
@@ -31,6 +33,8 @@ function create_key_handler({
 	);
 	return Object.assign({code}, callbacks);
 }
+
+export const KeyHandler = create_key_handler;
 
 function keydown_handler(key_handlers, emitter) {
 	const handle = cond(
@@ -79,77 +83,73 @@ function keypress_handler(key_handlers, emitter) {
 	}
 }
 
-export function Keyboard() {
-	const emitter = new EventEmitter();
-	const state = {
-		keydownEventHandler: null,
-		keydupEventHandler: null,
-		keypressEventHandler: null
-	};
-	const instance = Object.assign(emitter, {
-		use(handlers) {
-			document.removeEventListener('keydown', state.keydownEventHandler);
-			document.removeEventListener('keyup', state.keydupEventHandler);
-			document.removeEventListener('keypress', state.keypressEventHandler);
+const keys = {
+	get KEY_ENTER() {return  13;},
+	get KEY_SPACE() {return  32;},
+	get KEY_LEFT()  {return  37;},
+	get KEY_UP()    {return  38;},
+	get KEY_RIGHT() {return  39;},
+	get KEY_DOWN()  {return  40;},
+	get KEY_0()     {return  48;},
+	get KEY_1()     {return  49;},
+	get KEY_2()     {return  50;},
+	get KEY_3()     {return  51;},
+	get KEY_4()     {return  52;},
+	get KEY_5()     {return  53;},
+	get KEY_6()     {return  54;},
+	get KEY_7()     {return  55;},
+	get KEY_8()     {return  56;},
+	get KEY_9()     {return  57;},
+	get KEY_A()     {return  97;},
+	get KEY_B()     {return  98;},
+	get KEY_C()     {return  99;},
+	get KEY_D()     {return  100;},
+	get KEY_E()     {return  101;},
+	get KEY_F()     {return  102;},
+	get KEY_G()     {return  103;},
+	get KEY_H()     {return  104;},
+	get KEY_I()     {return  105;},
+	get KEY_J()     {return  106;},
+	get KEY_K()     {return  107;},
+	get KEY_L()     {return  108;},
+	get KEY_M()     {return  109;},
+	get KEY_N()     {return  110;},
+	get KEY_O()     {return  111;},
+	get KEY_P()     {return  112;},
+	get KEY_Q()     {return  113;},
+	get KEY_R()     {return  114;},
+	get KEY_S()     {return  115;},
+	get KEY_T()     {return  116;},
+	get KEY_U()     {return  117;},
+	get KEY_V()     {return  118;},
+	get KEY_W()     {return  119;},
+	get KEY_X()     {return  120;},
+	get KEY_Y()     {return  121;},
+	get KEY_Z()     {return  122;},
+};
 
-			state.keydownEventHandler = keydown_handler(handlers, emitter);
-			state.keydupEventHandler = keyup_handler(handlers, emitter);
-			state.keypressEventHandler = keypress_handler(handlers, emitter);
+const emitter = new EventEmitter();
+const state = {
+	keydownEventHandler: null,
+	keydupEventHandler: null,
+	keypressEventHandler: null
+};
 
-			document.addEventListener('keydown', state.keydownEventHandler);
-			document.addEventListener('keyup', state.keydupEventHandler);
-			document.addEventListener('keypress', state.keypressEventHandler);
-		}
-	});
+export const keyboard = completeAssign(emitter, keys, {
+	use(handlers) {
+		document.removeEventListener('keydown', state.keydownEventHandler);
+		document.removeEventListener('keyup', state.keydupEventHandler);
+		document.removeEventListener('keypress', state.keypressEventHandler);
 
-	Object.defineProperties(instance, {
-		'KEY_ENTER': {value: 13},
-		'KEY_SPACE': {value: 32},
-		'KEY_LEFT':  {value: 37},
-		'KEY_UP':    {value: 38},
-		'KEY_RIGHT': {value: 39},
-		'KEY_DOWN':  {value: 40},
-		'KEY_0':     {value: 48},
-		'KEY_1':     {value: 49},
-		'KEY_2':     {value: 50},
-		'KEY_3':     {value: 51},
-		'KEY_4':     {value: 52},
-		'KEY_5':     {value: 53},
-		'KEY_6':     {value: 54},
-		'KEY_7':     {value: 55},
-		'KEY_8':     {value: 56},
-		'KEY_9':     {value: 57},
-		'KEY_A':     {value: 97},
-		'KEY_B':     {value: 98},
-		'KEY_C':     {value: 99},
-		'KEY_D':     {value: 100},
-		'KEY_E':     {value: 101},
-		'KEY_F':     {value: 102},
-		'KEY_G':     {value: 103},
-		'KEY_H':     {value: 104},
-		'KEY_I':     {value: 105},
-		'KEY_J':     {value: 106},
-		'KEY_K':     {value: 107},
-		'KEY_L':     {value: 108},
-		'KEY_M':     {value: 109},
-		'KEY_N':     {value: 110},
-		'KEY_O':     {value: 111},
-		'KEY_P':     {value: 112},
-		'KEY_Q':     {value: 113},
-		'KEY_R':     {value: 114},
-		'KEY_S':     {value: 115},
-		'KEY_T':     {value: 116},
-		'KEY_U':     {value: 117},
-		'KEY_V':     {value: 118},
-		'KEY_W':     {value: 119},
-		'KEY_X':     {value: 120},
-		'KEY_Y':     {value: 121},
-		'KEY_Z':     {value: 122},
+		state.keydownEventHandler = keydown_handler(handlers, emitter);
+		state.keydupEventHandler = keyup_handler(handlers, emitter);
+		state.keypressEventHandler = keypress_handler(handlers, emitter);
 
-		'createKeyHandler': {value: create_key_handler}
-	});
+		document.addEventListener('keydown', state.keydownEventHandler);
+		document.addEventListener('keyup', state.keydupEventHandler);
+		document.addEventListener('keypress', state.keypressEventHandler);
+	},
+	createKeyHandler: KeyHandler
+});
 
-	return instance;
-}
-
-export default Keyboard();
+export default keyboard;
--- tests/ui/keyboard_spec.js
+++ tests/ui/keyboard_spec.js
@@ -1,9 +1,9 @@
-import keyboard from 'ui/keyboard';
+import {default as keyboard, KeyHandler} from 'ui/keyboard';
 import {expect} from 'chai';
 import sinon from 'sinon';
 import jsdom from 'jsdom';
 
-describe('keyboard.createKeyHandler(code, ev, on_keydown, on_keyup, repeat)', () => {
+describe('KeyHandler(code, ev, on_keydown, on_keyup, repeat)', () => {
 	const code = 0;
 	const event = 'event';
 	const on_keydown = sinon.stub();
@@ -15,20 +15,20 @@ describe('keyboard.createKeyHandler(code, ev, on_keydown, on_keyup, repeat)', ()
 	});
 
 	it('returns an object width a code attribute wicth is a number', () => {
-		const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+		const kh = KeyHandler({code, event, on_keydown, on_keyup});
 		expect(kh.code).to.be.a('number');
 	});
 	it('returns an object width a keydown attribute wicth is a function', () => {
-		const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+		const kh = KeyHandler({code, event, on_keydown, on_keyup});
 		expect(kh.keydown).to.be.a('function');
 	});
 	it('returns an object width a keydup attribute wicth is a function', () => {
-		const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+		const kh = KeyHandler({code, event, on_keydown, on_keyup});
 		expect(kh.keyup).to.be.a('function');
 	});
 });
 
-describe('keyboardHandler', () => {
+describe('KeyboardHandler', () => {
 	const code = 0;
 	const event = 'event';
 	const on_keydown = sinon.stub();
@@ -44,42 +44,42 @@ describe('keyboardHandler', () => {
 
 	describe('code', () => {
 		it('is equal to the code passed at creation', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup});
 			expect(kh.code).to.equal(code);
 		});
 	});
 
 	describe('keydown(v)', () => {
 		it('calls back on_keydown each time it is invoked if repeat is set to true', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup});
 			kh.keydown();
 			kh.keydown();
 			expect(on_keydown.calledTwice).to.be.true;
 		});
 		it('calls back on_keydown once when invoked one or many times if repeat is set to false and keyup has not been invoked', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup, repeat: false});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup, repeat: false});
 			kh.keydown();
 			kh.keydown();
 			expect(on_keydown.calledOnce).to.be.true;
 		});
 		it('forwards its argument to on_keydown', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup, repeat: false});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup, repeat: false});
 			const arg = {};
 			kh.keydown(arg);
 			expect(on_keydown.calledWith(arg)).to.be.true;
 		});
 		it('returns an object with an event attribute set with the value passed at creation', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup, repeat: false});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup, repeat: false});
 			const o = kh.keydown();
 			expect(o.event).to.equal('event');
 		});
 		it('returns an object with a data attribute set with returned value of on_keydown', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup, repeat: false});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup, repeat: false});
 			const o = kh.keydown();
 			expect(o.data).to.equal(0);
 		});
 		it('returns an object with no data attribute set if on_keydown does not return any value', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown: () => {}, on_keyup, repeat: false});
+			const kh = KeyHandler({code, event, on_keydown: () => {}, on_keyup, repeat: false});
 			const o = kh.keydown();
 			expect(o.data).to.be.undefined;
 		});
@@ -87,40 +87,40 @@ describe('keyboardHandler', () => {
 
 	describe('keyup(v)', () => {
 		it('calls back on_keyup each time it is invoked if repeat is set to true', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup});
 			kh.keyup();
 			kh.keyup();
 			expect(on_keyup.calledTwice).to.be.true;
 		});
 		it('calls back on_keyup each time it is invoked if repeat is set to false', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup, repeat: false});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup, repeat: false});
 			kh.keyup();
 			kh.keyup();
 			expect(on_keyup.calledTwice).to.be.true;
 		});
 		it('forwards its argument to on_keyup', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup});
 			const arg = {};
 			kh.keyup(arg);
 			expect(on_keyup.calledWith(arg)).to.be.true;
 		});
 		it('returns an object with an event attribute set with the value passed at creation', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup});
 			const o = kh.keydown();
 			expect(o.event).to.equal('event');
 		});
 		it('returns an object with a data attribute set with returned value of on_keyup', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup});
 			const o = kh.keyup();
 			expect(o.data).to.equal(0);
 		});
 		it('returns an object with no data attribute set if on_keyup does not return any value', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup: () => {}});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup: () => {}});
 			const o = kh.keyup();
 			expect(o.data).to.be.undefined;
 		});
 		it('enabled on_keydown to be invoked again if repeat has been disabled', () => {
-			const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup, repeat: false});
+			const kh = KeyHandler({code, event, on_keydown, on_keyup, repeat: false});
 			kh.keydown();
 			kh.keydown();
 			kh.keyup();
@@ -145,21 +145,21 @@ describe('Keyboard.use([{}])', () => {
 	});
 
 	it('calls on_keydown when the key is pressed', () => {
-		const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+		const kh = KeyHandler({code, event, on_keydown, on_keyup});
 		keyboard.use([kh]);
 		document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: code}));
 		expect(on_keydown).to.have.been.calledOnce;
 	});
 
 	it('does not call on_keydown when an other key is pressed', () => {
-		const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+		const kh = KeyHandler({code, event, on_keydown, on_keyup});
 		keyboard.use([kh]);
 		document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: code + 1}));
 		expect(on_keydown).to.not.have.been.called;
 	});
 
 	it('calls on_keydup when the key is released', () => {
-		const kh = keyboard.createKeyHandler({code, event, on_keydown, on_keyup});
+		const kh = KeyHandler({code, event, on_keydown, on_keyup});
 		keyboard.use([kh]);
 		document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: code}));
 		expect(on_keyup).to.have.been.calledOnce;
