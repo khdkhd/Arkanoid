f5e2728e426383a817a3e7bbc1a7fd097a33b2be
NealRame <contact@nealrame.com>
2017-01-08 17:58:02 +0100

    add a TODO comment

--- sources/js/graphics/bounding-box.js
+++ sources/js/graphics/bounding-box.js
@@ -1,6 +1,7 @@
 import Rect from 'maths/rect';
 import Vector from 'maths/vector';
 
+// TODO move the align_center_to_origin parameter as a field into state
 export default function BoundingBox(state, align_center_to_origin = false) {
 	const origin = align_center_to_origin
 		? pos => pos.add({x: -state.size.width/2, y: -state.size.height/2})
