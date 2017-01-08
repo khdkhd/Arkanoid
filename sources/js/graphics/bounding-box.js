c8a951318c494d2560c75837fbb53a41f7e1ca12
NealRame <contact@nealrame.com>
2017-01-08 19:34:35 +0100

    move align flag to argument object

--- sources/js/graphics/bounding-box.js
+++ sources/js/graphics/bounding-box.js
@@ -1,16 +1,15 @@
 import Rect from 'maths/rect';
 import Vector from 'maths/vector';
 
-// TODO move the align_center_to_origin parameter as a field into state
-export default function BoundingBox(state, align_center_to_origin = false) {
-	const origin = align_center_to_origin
-		? pos => pos.add({x: -state.size.width/2, y: -state.size.height/2})
+export default function BoundingBox({position, size, alignCenterToOrigin = false}) {
+	const origin = alignCenterToOrigin
+		? pos => pos.add({x: -size.width/2, y: -size.height/2})
 		: pos => pos;
-	const relative_bbox = Rect(origin(Vector.Null), state.size);
+	const relative_bbox = Rect(origin(Vector.Null), size);
 	return {
 		boundingBox: {
 			get absolute() {
-				return Rect(origin(state.position), state.size);
+				return Rect(origin(position), size);
 			},
 			get relative() {
 				return relative_bbox;
