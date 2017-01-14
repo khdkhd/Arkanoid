02145c7970cb3806fb906673dfb4c399f51f7766
NealRame <contact@nealrame.com>
2017-01-14 13:42:41 +0100

    fix grid component

--- sources/js/graphics/grid.js
+++ sources/js/graphics/grid.js
@@ -1,21 +1,20 @@
 import SceneObject from 'graphics/scene-object';
 
-import {EventEmitter} from 'events';
 import times from 'lodash.times';
 
 export default function Grid(cols, rows, step, color) {
 	return SceneObject({
-		zIndex: -Infinity,
-		emitter: new EventEmitter(),
-		onRender(screen) {
+		onRender(scene) {
+			const {screen, scale} = scene;
+
 			screen.pen = {
 				strokeStyle: color,
-				lineWidth: 1/16
+				lineWidth: 1/scale
 			};
 
 			// Vertical guides
 			screen.save();
-			screen.translate({x: .5/16, y: .5/16});
+			screen.translate({x: .5/scale, y: .5/scale});
 			times(cols, () => {
 				screen.translate({x: step, y: 0});
 				screen.drawLine({x: 0, y: 0}, {x: 0, y: rows*step});
@@ -29,6 +28,12 @@ export default function Grid(cols, rows, step, color) {
 				screen.drawLine({x: 0, y: 0}, {x: cols*step, y: 0});
 			});
 			screen.restore();
-		}
+		},
+		position: {x: 0, y: 0},
+		size: {
+			width: cols*step,
+			height: rows*step
+		},
+		zIndex: -Infinity
 	});
 }
