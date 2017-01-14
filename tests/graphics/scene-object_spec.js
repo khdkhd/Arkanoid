fdd93fc0b56c05e8d188a6b1d783eba7d7a980a5
NealRame <contact@nealrame.com>
2017-01-14 13:22:18 +0100

    update SceneObject tests

--- tests/graphics/scene-object_spec.js
+++ tests/graphics/scene-object_spec.js
@@ -20,87 +20,100 @@ function SceneMock() {
 	), {screen: Screen(CanvasContextMock())});
 }
 
-describe('graphics.SceneObject({[onSceneChanged,] onRender, zIndex})', () => {
+describe('graphics.SceneObject({position, size, [, onSceneChanged][, onRender][, zIndex]})', () => {
 	const zIndex = 42;
-	const position = Vector.Null;
+	const position = Vector({x: 1, y: 1});
 	const size = {
-		width: 0,
-		height: 0
+		width: 1,
+		height: 1
 	};
+	describe('#boundingBox.absolute', () => {
+		it('is a maths.Rect', () => {
+			const scene_object = SceneObject({position, size});
+			const r = scene_object.boundingBox.absolute;
+			expect(r.x).to.equal(position.x);
+			expect(r.y).to.equal(position.y);
+			expect(r.width).to.equal(size.width);
+			expect(r.height).to.equal(size.height);
+		});
+	});
+	describe('#boundingBox.relative', () => {
+		it('is a maths.Rect with its top left point equals to (0, 0)', () => {
+			const scene_object = SceneObject({position, size});
+			const r = scene_object.boundingBox.relative;
+			expect(r.x).to.equal(0);
+			expect(r.y).to.equal(0);
+			expect(r.width).to.equal(size.width);
+			expect(r.height).to.equal(size.height);
+		});
+	});
 	describe('#scene', () => {
-		it('sets the scene when affected a value', () => {
-			const onRender = () => {};
-			const scene_object = SceneObject({position, size, zIndex, onRender});
+		it('returns the scene', () => {
 			const scene = SceneMock();
-			scene_object.scene = scene;
+			const scene_object = SceneObject({position, size, scene});
 			expect(scene_object.scene).to.equal(scene);
 		});
+		it('sets the scene when affected a value', () => {
+			const scene1 = SceneMock();
+			const scene2 = SceneMock();
+			const scene_object = SceneObject({position, size, scene1});
+			scene_object.scene = scene2;
+			expect(scene_object.scene).to.equal(scene2);
+		});
+		it('adds itself to the scene when affected a value', () => {
+			const scene_object = SceneObject({position, size});
+			const scene = SceneMock();
+			scene_object.scene = scene;
+			expect(scene.add).to.have.been.calledOnce;
+			expect(scene.add).to.have.been.calledWith(scene_object);
+		});
+		it('removes itself from the scene when affected null or undefined', () => {
+			const scene = SceneMock();
+			const scene_object = SceneObject({position, size, scene});
+			scene_object.scene = null;
+			expect(scene.remove).to.have.been.calledOnce;
+			expect(scene.remove).to.have.been.calledWith(scene_object);
+		});
 		it('calls back onSceneChanged with scene when affected a value', () => {
-			const onRender = () => {};
 			const onSceneChanged = sinon.spy();
-			const scene_object = SceneObject({position, size, zIndex, onRender, onSceneChanged});
+			const scene_object = SceneObject({position, size, onSceneChanged});
 			const scene = SceneMock();
 			scene_object.scene = scene;
 			expect(onSceneChanged).to.have.been.calledOnce;
 			expect(onSceneChanged).to.have.been.calledWith(scene);
 		});
-		it('adds itself to the scene when affected a value', () => {
-			const onRender = () => {};
-			const scene_object = SceneObject({position, size, zIndex, onRender});
-			const scene = SceneMock();
-			scene_object.scene = scene;
-			expect(scene.add).to.have.been.called;
-		});
-		it('removes itself from the scene when affected null or undefined', () => {
-			const onRender = () => {};
-			const scene_object = SceneObject({position, size, zIndex, onRender});
-			const scene = SceneMock();
-			scene_object.scene = scene;
-			scene_object.scene = null;
-			expect(scene.remove).to.have.been.called;
-		});
-		it('returns the scene', () => {
-			const onRender = () => {};
-			const scene_object = SceneObject({position, size, zIndex, onRender});
-			const scene = SceneMock();
-			scene_object.scene = scene;
-			expect(scene_object.scene).to.equal(scene);
-		});
 	});
 	describe('#zIndex', () => {
+		it('returns the z-index', () => {
+			const scene_object = SceneObject({position, size, zIndex});
+			expect(scene_object.zIndex).to.equal(zIndex);
+		});
 		it('sets the z-index when affected a value', () => {
-			const onRender = () => {};
-			const scene_object = SceneObject({position, size, zIndex, onRender});
+			const scene_object = SceneObject({position, size, zIndex});
 			scene_object.zIndex = zIndex + 1;
 			expect(scene_object.zIndex).to.equal(zIndex + 1);
 		});
 		it('re-adds itself to the scene when affected a value', () => {
-			const onRender = () => {};
-			const scene_object = SceneObject({position, size, zIndex, onRender});
+			const scene_object = SceneObject({position, size, zIndex});
 			scene_object.scene = SceneMock();
 			scene_object.zIndex = zIndex + 1;
 			expect(scene_object.scene.add).to.have.been.called;
-		});
-		it('returns the z-index', () => {
-			const onRender = () => {};
-			const scene_object = SceneObject({position, size, zIndex, onRender});
-			expect(scene_object.zIndex).to.equal(zIndex);
 		});
 	});
 	describe('#render()', () => {
 		it('calls back onRender with the scene as parameter if rendering is enabled', () => {
 			const onRender = sinon.spy();
-			const scene_object = SceneObject({position, size, zIndex, onRender});
-			scene_object.scene = SceneMock();
+			const scene = SceneMock();
+			const scene_object = SceneObject({position, onRender, scene, size, zIndex});
 			scene_object.toggleRender(true);
 			scene_object.render();
 			expect(onRender).to.have.been.calledOnce;
-			expect(onRender).to.have.been.calledWith(scene_object.scene);
+			expect(onRender).to.have.been.calledWith(scene);
 		});
 		it('does not calls back onRender if rendering is disabled', () => {
 			const onRender = sinon.spy();
-			const scene_object = SceneObject({position, size, zIndex, onRender});
-			scene_object.scene = SceneMock();
+			const scene = SceneMock();
+			const scene_object = SceneObject({position, onRender, scene, size, zIndex});
 			scene_object.toggleRender(false);
 			scene_object.render();
 			expect(onRender).to.not.have.been.called;
