788123297f04621d9ecfe692e17d9ee1cdb608ed
a-cordier <com.acordier@gmail.com>
2017-01-22 19:52:38 +0100

    refacto synth core

--- sources/js/sound/synth/core.js
+++ sources/js/sound/synth/core.js
@@ -1,35 +1,38 @@
-import synthFactory from 'sound/synth/factory';
-import controlFactory from 'sound/controls/factory';
-import is_nil from 'lodash.isnil';
+import SynthFactory from 'sound/synth/factory';
+import ControlFactory from 'sound/controls/factory';
 import {get_frequency_of_note } from 'sound/common/utils';
+
 
 export default ({audio_context})  => {
 		const voices = [];
 		const views = [];
 		let output, mods;
+
+		function bind_views(param, view_defs = []){
+			view_defs.forEach(view_def => {
+				const view = ControlFactory[view_def.factory](view_def.options);
+				view.param = param;
+				views.push(view);
+			});
+		}
+
 		return {
 			patch(patch) {
 				mods = patch.nodes.reduce((mods, node)=> {
 					const options = Object.assign({}, node.options)
-					options.factory = synthFactory;
-					mods[node.id] = options.factory[node.factory](Object.assign({audio_context}, options));
-					const config = node.config;
-					Object.keys(config).forEach(param => {
-						mods[node.id][param].value = config[param].value;
-						if(!is_nil(config[param].views)){
-							config[param].views.forEach(view_def =>{
-								const view = controlFactory[view_def.factory](view_def.options);
-								view.param = mods[node.id][param];
-								views.push(view);
-							});
-						}
-					});
+					options.factory = SynthFactory;
+					const mod = options.factory[node.factory](Object.assign({audio_context}, options));
+					for(let [param, config] of Object.entries(node.config)){
+						mod[param].value = config.value;
+						bind_views(mod[param], config.views);
+					}
 					if(node.voice){
-						voices.push(mods[node.id]);
+						voices.push(mod);
 					}
 					if(node.output){
-						output = mods[node.id];
+						output = mod;
 					}
+					mods[node.id] = mod;
 					return mods;
 				}, {});
 				for(let con of patch.connexions){
@@ -37,12 +40,12 @@ export default ({audio_context})  => {
 				}
 			},
 			noteOn(note, octave, time) {
-					voices.forEach(function(generator){
+					voices.forEach((generator) => {
 					generator.noteOn(get_frequency_of_note(note, octave), time);
 				});
 			},
 			noteOff(note, octave, time){
-				voices.forEach(function(generator){
+				voices.forEach((generator) => {
 					generator.noteOff(get_frequency_of_note(note, octave), time);
 				});
 			},
