6bc10d45ee6912be107bd7da01a150bbfe3b5974
NealRame <contact@nealrame.com>
2017-01-29 23:05:10 +0100

    add the possibility to only run wanted tests

--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -4,8 +4,17 @@ source "$PWD/tools/scripts/env"
 
 REPORTER=${REPORTER:-"spec"}
 
-mocha \
-	--compilers js:babel-core/register \
-	--require "$PWD/tests/chai-helpers.js" \
-	--recursive "$PWD/tests" \
-	--reporter "$REPORTER"
+if [ -z "$*" ];
+then
+	mocha \
+		--compilers js:babel-core/register \
+		--require "$PWD/tests/chai-helpers.js" \
+		--recursive "$PWD/tests" \
+		--reporter "$REPORTER"
+else
+	mocha \
+		--compilers js:babel-core/register \
+		--require "$PWD/tests/chai-helpers.js" \
+		--reporter "$REPORTER" \
+		"$@"
+fi
