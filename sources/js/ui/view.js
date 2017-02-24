3237108f2bad08ffeef585fd31733de3622dc7b2
NealRame <contact@nealrame.com>
2017-02-24 21:46:03 +0100

    Refactor View

--- sources/js/ui/view.js
+++ sources/js/ui/view.js
@@ -1,42 +1,110 @@
 import is_nil from 'lodash.isnil';
+import noop from 'lodash.noop';
+import uniq from 'lodash.uniq';
 
-export default ({el = null, model = null, onRender} = {}) => {
-	const state = {
-		el,
-		model
-	};
-	const render = () => {
-		if (!is_nil(state.el)) {
-			state.el.innerHTML = '';
+export function defaultOnBeforeRender(el) {
+	el.innerHTML = '';
+}
+
+export function defaultOnDestroy(el) {
+	el.remove();
+}
+
+export function createElement({el, attributes, classNames, id, tagName}) {
+	if (is_nil(el)) {
+		el = document.createElement(tagName);
+		el.className = uniq(classNames).join(' ');
+		el.id = id;
+		for (let [key, value] of Object.entries(attributes)) {
+			el.setAttribute(key, value);
+		}
+	}
+	return el;
+}
+
+export function createEventsManager(state, render) {
+	const events_handlers = Object.entries(state.events).map(([event, handler]) => ([
+		event,
+		ev => handler(ev, state.el, state.model)
+	]));
+	return {
+		connect() {
+			if (!state.connected) {
+				state.connected = true;
+				for (let [event, handler] of events_handlers) {
+					state.el.addEventListener(event, handler);
+				}
+				if (!is_nil(state.model)) {
+					state.model.on('changed', render);
+				}
+			}
+		},
+		disconnect() {
+			state.connected = false;
+			for (let [event, handler] of events_handlers) {
+				state.el.removeEventListener(event, handler);
+			}
 			if (!is_nil(state.model)) {
-				onRender(el, state.model);
+				state.model.removeListener('changed', render);
 			}
 		}
 	};
-	return {
-		attach(el) {
-			state.el = el;
-			return this;
+}
+
+export default ({
+	el = null,
+	classNames = [],
+	id = '',
+	tagName = 'div',
+	attributes = {},
+	events = {},
+	model = null,
+	onBeforeDestroy = noop,
+	onBeforeRender = defaultOnBeforeRender,
+	onRender = noop
+} = {}) => {
+	const state = {
+		connected: false,
+		el: createElement({el, attributes, classNames, id, tagName}),
+		events,
+		model
+	};
+	const render = () => {
+		const {el, model} = state;
+		onBeforeRender(el, model);
+		onRender(el, model);
+	};
+	const {connect, disconnect} = createEventsManager(state, render);
+	return ({
+		destroy() {
+			disconnect();
+			onBeforeDestroy(el, model);
+			state.el.remove();
+		},
+		el() {
+			return state.el;
 		},
 		model() {
 			return state.model;
 		},
 		setModel(model) {
-			// disconnect from previous model
-			if (!is_nil(state.model)) {
-				state.model.removeListener('changed', render);
-			}
-			// connect to new model
-			if (!is_nil(model)) {
-				model.on('changed', render);
-			}
+			disconnect();
 			state.model = model;
+			connect();
 			render();
 			return this;
 		},
 		render() {
 			render();
 			return this;
+		},
+		connect() {
+			connect();
+			return this;
+		},
+		disconnect() {
+			disconnect();
+			return this;
 		}
-	};
+	}).connect();
 }
