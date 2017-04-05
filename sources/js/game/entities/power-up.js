db7e5ba6063d80137f6d03e127b4820cf37d8548
NealRame <contact@nealrame.com>
2017-04-05 14:36:43 +0200

    Add missing pills

--- sources/js/game/entities/power-up.js
+++ sources/js/game/entities/power-up.js
@@ -73,18 +73,22 @@ const flare_box2 = new Path2D(`
 
 export function PowerUpModel({x, y}, type) {
 	const color = ({
+		[PowerUp.Break]:     hsl(292,  84, 44), // #b612cf
+		[PowerUp.Catch]:     hsl(120, 100, 20), // #006600
+		[PowerUp.Expand]:    hsl(249, 100, 33), // #1c00ab
+		[PowerUp.ExtraLife]: hsl(  0,   0, 40), // #666666
 		[PowerUp.Laser]:     hsl(  0,  99, 39), // #c60101
 		[PowerUp.Slow]:      hsl( 24, 100, 50), // #ff6600
-		[PowerUp.ExtraLife]: hsl(  0,   0, 40), // #666666
-		[PowerUp.Expand]:    hsl(187, 100, 36), // #00a2b8
-		[PowerUp.Catch]:     hsl(120, 100, 20)  // #006600
+		[PowerUp.Split]:     hsl(213,  96, 70), // #6aaafc
 	})[type];
 	const letter = ({
+		[PowerUp.Break]:     'B',
+		[PowerUp.Catch]:     'C',
+		[PowerUp.Expand]:    'E',
+		[PowerUp.ExtraLife]: 'P',
 		[PowerUp.Laser]:     'L',
 		[PowerUp.Slow]:      'S',
-		[PowerUp.ExtraLife]: 'P',
-		[PowerUp.Expand]:    'E',
-		[PowerUp.Catch]:     'C'
+		[PowerUp.Split]:     'D'
 	})[type];
 	const coordinates = VerletModel({
 		width: 2,
@@ -177,6 +181,26 @@ export default function PowerUp({x, y}, type) {
 	);
 }
 
+Object.defineProperty(PowerUp, 'Break', {
+	writable: false,
+	value: Symbol('PowerUp.Break')
+});
+
+Object.defineProperty(PowerUp, 'Catch', {
+	writable: false,
+	value: Symbol('PowerUp.Catch')
+});
+
+Object.defineProperty(PowerUp, 'Expand', {
+	writable: false,
+	value: Symbol('PowerUp.Expand')
+});
+
+Object.defineProperty(PowerUp, 'ExtraLife', {
+	writable: false,
+	value: Symbol('PowerUp.ExtraLife')
+});
+
 Object.defineProperty(PowerUp, 'Laser', {
 	writable: false,
 	value: Symbol('PowerUp.Laser')
@@ -187,17 +211,7 @@ Object.defineProperty(PowerUp, 'Slow', {
 	value: Symbol('PowerUp.Slow')
 });
 
-Object.defineProperty(PowerUp, 'ExtraLife', {
+Object.defineProperty(PowerUp, 'Split', {
 	writable: false,
-	value: Symbol('PowerUp.ExtraLife')
-});
-
-Object.defineProperty(PowerUp, 'Expand', {
-	writable: false,
-	value: Symbol('PowerUp.Expand')
-});
-
-Object.defineProperty(PowerUp, 'Catch', {
-	writable: false,
-	value: Symbol('PowerUp.Catch')
+	value: Symbol('PowerUp.Split')
 });
