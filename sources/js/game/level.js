8578951b8fee1e54e275e12bf54358e051d2c390
NealRame <contact@nealrame.com>
2017-04-17 02:05:27 +0200

    Emit powerUp event when brick get destroyed

--- sources/js/game/level.js
+++ sources/js/game/level.js
@@ -1,9 +1,17 @@
 import {Collection} from 'model';
+import {PowerUpRandomizer} from 'game/entities/power-up';
+
+import is_nil from 'lodash.isnil';
 
 export default function Level() {
+	const maybePill = PowerUpRandomizer();
 	const collection = Collection();
 	collection.on('itemDestroyed', brick => {
 		if (brick.color() !== 'gold') {
+			const pill = maybePill(brick.position());
+			if (!is_nil(pill)) {
+				collection.emit('powerUp', pill);
+			}
 			if (collection.every(brick => brick.color() === 'gold')) {
 				collection.emit('completed');
 			}
