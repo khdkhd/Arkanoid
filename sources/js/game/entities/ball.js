7dd396f9bdcede094d89fd05e0495a1e5c825530
NealRame <contact@nealrame.com>
2017-04-21 21:54:27 +0200

    Refactoring

--- sources/js/game/entities/ball.js
+++ sources/js/game/entities/ball.js
@@ -1,33 +1,22 @@
+import {Model, Collection} from 'model';
+import SceneObject from 'graphics/scene-object';
 import Vector from 'maths/vector';
 import VerletModel from 'physics/verlet-model';
-import SceneObject from 'graphics/scene-object';
-import {EventEmitter} from 'events';
 
 const radius = .3;
 
-export function BallModel({x, y}) {
-	return {
-		emitter: new EventEmitter(),
-		radius,
-		verlet: VerletModel({
-			width: 2*radius,
-			height: 2*radius,
-		}, {x, y})
-	};
-}
-
-export function BallView(state) {
-	return SceneObject(state.verlet, {
+export function BallView({verlet}) {
+	return SceneObject(verlet, {
 		onRender(screen) {
 			const scale = screen.absoluteScale().x;
-			const center = state.verlet.localRect().center;
+			const center = verlet.localRect().center;
 			screen.brush = 'white';
 			screen.pen = {
 				strokeStyle: 'hsl(210, 50%, 50%)',
 				lineWidth: 1/scale
 			};
 			screen.beginPath();
-			screen.arc(center, state.radius, 0, 2*Math.PI, false);
+			screen.arc(center, radius, 0, 2*Math.PI, false);
 			screen.closePath();
 			screen.fillPath();
 			screen.drawPath();
@@ -45,14 +34,68 @@ export function BallController({verlet}) {
 	}, verlet);
 }
 
-export function Ball({x, y}) {
-	const state = BallModel({x, y});
-	return Object.assign(
-		state.emitter,
-		state.verlet,
-		BallView(state),
-		BallController(state)
+export function Ball(
+	{x: px, y: py} = Vector.Null,
+	{x: vx, y: vy} = Vector.Null
+) {
+	const verlet = VerletModel(
+		{width: 2*radius, height: 2*radius}, // size
+		{x: px, y: py}, // initial position
+		{x: vx, y: vy}  // initial speed
 	);
+	return Object.assign(
+		Model(),
+		verlet,
+		BallView({verlet}),
+		BallController({verlet})
+	);
+}
+
+export function BallCollection() {
+	const collection = Collection({ItemModel: Ball});
+	collection
+		.on('itemDestroyed', () => {
+			if (collection.size() === 0) {
+				collection.emit('empty');
+			}
+		})
+		.on('itemAdded', ball => {
+			ball.on('hit', type => collection.emit('hit', type, ball));
+		});
+	return Object.assign(collection, {
+		hide() {
+			collection.forEach(ball => ball.hide());
+			return this;
+		},
+		show() {
+			collection.forEach(ball => ball.show());
+			return this;
+		},
+		update() {
+			collection.forEach(ball => ball.update());
+			return this;
+		},
+		splitter(teta) {
+			const cos_teta = Math.cos(teta);
+			const sin_teta = Math.sin(teta);
+			const m1 = {
+				m11:  cos_teta, m12:  sin_teta,
+				m21: -sin_teta, m22:  cos_teta
+			};
+			const m2 = {
+				m11:  cos_teta, m12: -sin_teta,
+				m21:  sin_teta, m22:  cos_teta
+			};
+			return () => {
+				if (collection.size() === 1) {
+					const [ball] = collection;
+					const v = ball.velocity();
+					collection.create(ball.position(), v.transform(m1));
+					collection.create(ball.position(), v.transform(m2));
+				}
+			};
+		}
+	});
 }
 
 Ball.Radius = radius;
