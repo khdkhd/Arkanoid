8d62d28da40883bf528d31078bf53f8700207212
a-cordier <com.acordier@gmail.com>
2017-05-20 20:28:26 +0200

    wip first instrument

--- sources/js/sound/instruments/nsc-synth/core.js
+++ sources/js/sound/instruments/nsc-synth/core.js
@@ -2,25 +2,39 @@ import Mono from 'sound/synth/mono';
 import Filter from 'sound/synth/filter';
 import Enveloppe from 'sound/synth/enveloppe';
 import Lfo from 'sound/synth/lfo';
-import amp from 'sound/synth/amp';
+import Amp from 'sound/synth/amp';
+import Note from 'sound/sequencer/note';
 
-export default({audio_context}) => {
+export default function NscSynth({audio_context}) {
 
   const mono = Mono({audio_context});
   const filter = Filter({audio_context});
   const enveloppe = Enveloppe({audio_context});
   const lfo = Lfo({audio_context});
+  const amp = Amp({audio_context});
 
+  amp.gain.value = 1;
+  filter.frequency.value = 1;
   mono
     .connect(filter)
     .connect(amp);
   enveloppe.connect(mono);
   lfo.connect(filter);
-  
+
   return {
     mono,
     filter,
     enveloppe,
-    lfo
+    lfo,
+    amp,
+    noteOn(note, octave, time, velocity = 1){
+      mono.noteOn(Note.getFrequency(note, octave), time, velocity);
+    },
+    noteOff(note, octave, time){
+      mono.noteOff(time);
+    },
+    connect({input}){
+      amp.connect({input});
+    }
   }
 }
--- sources/js/sound/instruments/nsc-synth/index.js
+++ sources/js/sound/instruments/nsc-synth/index.js
@@ -1,16 +1,44 @@
 import Core from './core'
 
+import Knob from 'sound/ui/canvas/knob'
+// import Fader from 'sound/ui/canvas/fader2'
+import GroupView from 'sound/ui/common/group-view';
+
 export default({audio_context}) => {
-  const core = Core({audio_context})
 
-  const filterFreqKnob = CanvasView(Knob, {width: 50, height: 50});
-  const filterQKnob = CanvasView(Knob, {width: 50, height: 50});
-  const lfoFreqKnob = CanvasView(Knob, {width: 50, height: 50});
-  const lfoAmpKnob = CanvasView(Knob, {width: 50, height: 50});
+  const core = Core({audio_context});
 
-  return {
-    render(){
+  const filterFreqKnob = Knob({
+    width: 50,
+    height: 50
+  }).setParam(core.filter.frequency)
 
-    }
-  }
+  const filterQKnob = Knob({
+    width: 50,
+    height: 50
+  }).setParam(core.filter.Q);
+
+  const lfoFreqKnob = Knob({
+    width: 50,
+    height: 50
+  }).setParam(core.lfo.frequency);
+
+  const lfoAmpKnob = Knob({
+    width: 50,
+    height: 50
+  }).setParam(core.lfo.amplitude);
+
+
+  const filterView = GroupView({
+    classNames: ['row', 'nsc-filter'],
+    tagName: 'div'
+  })
+  .add(filterFreqKnob)
+  .add(filterQKnob)
+  .add(lfoFreqKnob)
+  .add(lfoAmpKnob)
+
+  document.querySelector('body').appendChild(filterView.render().el());
+
+  return core;
 }
