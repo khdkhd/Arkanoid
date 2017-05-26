54293696ed60e9b56ab847695a32cb570c944d5d
a-cordier <com.acordier@gmail.com>
2017-05-26 18:11:32 +0200

    remove unused files

--- sources/js/sound/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Sequencer } from 'sound/sequencer/sequencer';
-export { default as Synth } from 'sound/synth';
-export { default as Mixer } from 'sound/audio/mixer';
-export { default as createKeyboard } from 'sound/keyboard/keyboard';
