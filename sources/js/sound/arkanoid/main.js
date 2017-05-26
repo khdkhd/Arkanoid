30a7d1fd9a5c7007a5fe8cc72b433054d33f4230
a-cordier <com.acordier@gmail.com>
2017-05-26 18:04:17 +0200

    handling midifile

--- sources/js/sound/arkanoid/main.js
+++ sources/js/sound/arkanoid/main.js
@@ -7,37 +7,34 @@ const fileInput = document.querySelector('#midi-file')
 
 const eventTypes = Object.assign({}, Meta, Status)
 
-
+const audio_context = new AudioContext();
+const synth = Synth({audio_context});
+const sequencer = Sequencer({audio_context})
+	.setTempo(230)
+	.setSlave('synth', synth);
+Mixer({audio_context})
+	.addTrack('synth', synth)
+	.connect({input:audio_context.destination});
 fileInput.addEventListener('change', function() {
 	const reader = new FileReader()
 	reader.onload = e => {
 		let data = new DataView(e.target.result, 0, e.target.result.byteLength)
 		let midiFile = MidiFile(data)
 		midiFile.tracks = midiFile.tracks
-			.map(track => {
-				track.events
-					.forEach((event) => {
+			.map((track) => {
+				let delta = 0;
+				return Object.assign(track, {
+					events: track.events.map((event) => {
 						event.type = Object.keys(eventTypes).find(key => eventTypes[key] === event.type)
+						event.time = delta + event.delta
+						delta += event.delta
+						return event
 					})
-				return track
-			})
-		// console.log(JSON.stringify(midiFile, undefined, 4))
+				})
+			});
+		const track = midiFile.tracks
+			.find(track => track.events.find(event => event.type === 'NOTE_ON') !== undefined)
+		sequencer.getTrack('synth').getPattern().setEvents(track.events)
 	}
 	reader.readAsArrayBuffer(this.files[0])
 })
-
-const audio_context = new AudioContext();
-const synth = Synth({audio_context});
-const sequencer = Sequencer({audio_context});
-sequencer.addSlave('synth', synth);
-const mixer = Mixer({audio_context});
-mixer.addTrack('synth', synth);
-mixer.connect({input:audio_context.destination});
-
-document.addEventListener('keydown', () => {
-	synth.noteOn('C', 4, audio_context.currentTime);
-}, true);
-
-document.addEventListener('keyup', () => {
-	synth.noteOff(null, null, audio_context.currentTime);
-}, true);
