8a6d43e5e022cc66a7019a759553c086d76d0a00
a-cordier <com.acordier@gmail.com>
2017-07-22 16:03:18 +0200

    Update amp node

--- sources/js/sound/synth/amp.js
+++ sources/js/sound/synth/amp.js
@@ -1,32 +1,31 @@
-import Model  from 'sound/common/model';
+import Model from 'sound/common/model';
 import constant from 'lodash.constant';
 
-export default({audio_context}) => {
+export default ({ audio_context }) => {
+  const _gain = audio_context.createGain();
 
-	const _gain = audio_context.createGain();
+  const gain = Model({
+    param: _gain.gain,
+    init: constant(0),
+    range: {
+      min: 0,
+      max: 1
+    }
+  });
 
-	const gain = Model({
-		param: _gain.gain,
-		init: constant(0),
-		range: {
-			min: 0,
-			max: 1
-		}
-	});
-
-	return {
-		connect({input, connect}) {
-			_gain.connect(input);
-			return {connect};
-		},
-		get input() {
-			return _gain;
-		},
-		get gain(){
-			return gain;
-		},
-		get param(){
-			return _gain.gain;
-		}
-	};
-}
+  return {
+    connect({ input, connect }) {
+      _gain.connect(input);
+      return { connect };
+    },
+    get input() {
+      return _gain;
+    },
+    get gain() {
+      return gain;
+    },
+    get param() {
+      return _gain.gain;
+    }
+  };
+};
